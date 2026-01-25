64d4dcbba1ece0add624cb071981bbad009cd5f7
Harsh Agnihotri <harshclekhak@gmail.com>
2026-01-25 18:38:00 +0530

    Updated java Programs

--- Java/JavaBasics.java
+++ Java/JavaBasics.java
@@ -1,5 +1,41 @@
-public class JavaBasics {
+// DAY 1
+
+// This is a simple Java program that prints "Hello, World!" to the console.
+
+/* public class JavaBasics {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
-}
+} */
+
+//variable : A variable is a container that holds data values , For example:
+
+/* public class JavaBasics {
+    public static void main(String[] args) {
+        int num1 = 10; // 'num1' is a variable of type int
+        int num2 = 20; // 'num2' is another variable of type int
+        int sum = num1 + num2; // 'sum' stores the result of adding
+        System.out.println("Sum: " + sum);
+    }
+} */
+
+//data types : Data types specify the type of data that a variable can hold. For example:
+
+/* public class JavaBasics {
+    public static void main(String[] args) {
+        int integerVar = 42; // Integer data type
+        double doubleVar = 3.14; // Double data type
+        char charVar = 'A'; // Character data type
+        String stringVar = "Hello, Java!"; // String data type
+        boolean boolVar = true; // Boolean data type
+
+        System.out.println("Integer: " + integerVar);
+        System.out.println("Double: " + doubleVar);
+        System.out.println("Character: " + charVar);
+        System.out.println("String: " + stringVar);
+        System.out.println("Boolean: " + boolVar);
+    }
+} */
+
+
+
